d0733ca0c74944316fb86a8e2effc115bdf4f65b
Victor Y. Fadeev <victor.y.fadeev@gmail.com>
2022-12-04 22:12:58 +0300

    Get all commits

--- GitlabImporter.py
+++ GitlabImporter.py
@@ -19,13 +19,13 @@ class GitlabImporter(Importer):
         for gl in self.repos:
             email = gl.user.commit_email
             for project in gl.projects.list(iterator=True, membership=True,
-                    last_activity_after=last_date.isoformat()):
+                    get_all=True, last_activity_after=last_date.isoformat()):
 
                 created_at = datetime.fromisoformat(
                     project.created_at.rstrip('Z'))
                 since = last_date if created_at < last_date else created_at
                 for commit in project.commits.list(iterator=True, all=True,
-                        since=since.isoformat()):
+                        get_all=True, since=since.isoformat()):
 
                     if commit.author_email == email:
                         commit.committed_date = datetime.fromisoformat(
@@ -43,7 +43,7 @@ class GitlabImporter(Importer):
     def get_changes(self, commit, stats):
         ''' for a specific commit it gets all the changed files '''
 
-        for diff in commit.diff(iterator=True):
+        for diff in commit.diff(iterator=True, get_all=True):
             ext = pathlib.Path(diff['new_path']).suffix
             if ext not in self.ignored_file_types:
                 insertions = diff['diff'].count('\n+')
