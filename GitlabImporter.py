b3e75ba93bd6d5b7fd875f39a31cdb4697a67544
Victor Y. Fadeev <victor.y.fadeev@gmail.com>
2022-12-04 14:07:31 +0300

    Add subclass for GitLab importing

---
+++ GitlabImporter.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python3
+
+import pathlib
+from datetime import datetime
+from miromannino.git_contributions_importer import *
+
+
+class GitlabImporter(Importer):
+
+    def get_all_commits(self, ignore_before_date):
+        ''' iter commits coming from any repos '''
+
+        last_date = datetime.fromtimestamp(ignore_before_date
+            if not self.ignore_before_date
+                or self.ignore_before_date < ignore_before_date
+            else self.ignore_before_date)
+
+        for gl in self.repos:
+            email = gl.user.commit_email
+            for project in gl.projects.list(iterator=True, membership=True,
+                    last_activity_after=last_date.isoformat()):
+
+                created_at = datetime.fromisoformat(
+                    project.created_at.rstrip('Z'))
+                since = last_date if created_at < last_date else created_at
+                for commit in project.commits.list(iterator=True, all=True,
+                        since=since.isoformat()):
+
+                    if commit.author_email == email:
+                        commit.committed_date = datetime.fromisoformat(
+                            commit.committed_date).timestamp()
+                        yield commit
+
+    def get_changes(self, commit, stats):
+        ''' for a specific commit it gets all the changed files '''
+
+        for diff in commit.diff(iterator=True):
+            ext = pathlib.Path(diff['new_path']).suffix
+            if ext not in self.ignored_file_types:
+                insertions = diff['diff'].count('\n+')
+                if insertions > 0:
+                    stats.add_insertions(ext, insertions)
+
+            ext = pathlib.Path(diff['old_path']).suffix
+            if ext not in self.ignored_file_types:
+                deletions = diff['diff'].count('\n-')
+                if deletions > 0:
+                    stats.add_deletions(ext, deletions)
+
+    def set_author(self, author):
+        pass
