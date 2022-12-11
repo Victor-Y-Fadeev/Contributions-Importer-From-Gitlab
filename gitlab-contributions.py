d51e02286d2870068597bde222cea935916cc238
Victor Y. Fadeev <victor.y.fadeev@gmail.com>
2022-12-12 00:10:31 +0300

    Support multi servers

--- gitlab-contributions.py
+++ gitlab-contributions.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
 
 import os
-import git
-import gitlab
+from git import Repo, rmtree
 from github import Github
+from gitlab import Gitlab
 from GitlabImporter import GitlabImporter
 
 
-GITLAB_SERVER_URL = os.environ['GITLAB_SERVER_URL']
-GITLAB_TOKEN = os.environ['GITLAB_TOKEN']
+GITLAB_SERVER_URL = os.environ['GITLAB_SERVER_URL'].split()
+GITLAB_TOKEN = os.environ['GITLAB_TOKEN'].split()
 GITHUB_TOKEN = os.environ['GITHUB_TOKEN']
 
 GITHUB_USER_LOGIN = Github(GITHUB_TOKEN).get_user().login
@@ -26,20 +26,25 @@ GITHUB_REPOSITORY_NAME = '{}/{}/{}'.format(GITHUB_SERVER_URL,
 
 
 def main():
-    repo = git.Repo.clone_from(url=GITHUB_REPOSITORY_NAME, to_path='mock',
-                               multi_options=['--depth 1'])
+    repo = Repo.clone_from(url=GITHUB_REPOSITORY_NAME, to_path='mock',
+                           multi_options=['--depth 1'])
     repo.config_writer().set_value('user', 'name',
                                    GITHUB_USER_NAME).release()
     repo.config_writer().set_value('user', 'email',
                                    GITHUB_USER_EMAIL).release()
 
-    gl = gitlab.Gitlab(GITLAB_SERVER_URL, private_token=GITLAB_TOKEN)
-    gl.auth()
+    server = [Gitlab(GITLAB_SERVER_URL[i], private_token=GITLAB_TOKEN[i])
+              for i in range(len(GITLAB_SERVER_URL))]
+    for gl in server:
+        gl.auth()
 
-    importer = GitlabImporter([gl], repo)
+    importer = GitlabImporter(server, repo)
     importer.set_max_commits_per_day([20, 20])
     importer.set_start_from_last(True)
     importer.import_repository()
+
+    repo.close()
+    rmtree('mock')
 
 if __name__ == '__main__':
     main()
